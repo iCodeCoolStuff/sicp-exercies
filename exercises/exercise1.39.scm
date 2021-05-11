5b43e1d6a8f770bbce7412d3bfeb8f2a98aeb0ce
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2021-05-11 15:57:12 -0400

    add an interative implementation cuz why not

--- exercises/exercise1.39.scm
+++ exercises/exercise1.39.scm
@@ -5,9 +5,19 @@
   (tan-cf-i 1)
 )
 
-
 (define (ex x i)
   (if (= i 1) x (square x)))
 
 (define (odd-nth x)
   (+ 1.0 (* 2 (- x 1))))
+
+(define (tan-cf-iter x k)
+  (define (tan-i i result)
+    (if (= i 0) result (tan-i (- i 1) (/ (ex x i) (- (odd-nth i) result))))
+  )
+  (tan-i k (/ (ex x k) (odd-nth k)))
+)
+
+(define (test)
+  (tan-cf-iter 2 10)
+)
