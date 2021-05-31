10f0c42348d7657cd228c37215b425d200b5ec03
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-05-31 17:53:59 -0400

    add exercise 2.16

---
+++ chapter-2/exercise2.16.scm
@@ -0,0 +1,5 @@
+; Equivalent algebraic expressions won't always yield the correct result because there is always
+; some expression that will yield a different result than expected.
+
+; It is impossible to devise an interval arithmetic package that does not have this shortcoming because it is generally
+; impossible to tell if two algebraic expressions are equivalent.
