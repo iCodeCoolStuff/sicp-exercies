104ee916c8a359142a0abf1d1c82d1e3b30592f0
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-06-01 09:06:27 -0400

    add exercise 2.22

---
+++ chapter-2/exercise2.22.scm
@@ -0,0 +1,5 @@
+; Part 1:
+; Cons adds the most recent element to the beginning of answer, so the returned list will be in reverse order.
+
+; Part 2:
+; Cons now conses answer into the car position instead of the cdr position.
