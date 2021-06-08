1009389bdfce4be12c39cdb2c1b7a5b5e727aed1
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-06-08 00:08:20 -0400

    add exercise 2.43

---
+++ chapter-2/exercise2.43.scm
@@ -0,0 +1,3 @@
+; Louis reasoner is running safe? on all of the boards that were already deemed to be safe. 
+;The program exercise 2.42 solves the problem in time T. However, the program in exercise 2.43 solves the problem
+; in time T^T because the program has to iterate over the new boards (time t) and all of the previous boards (T times T) as well.
