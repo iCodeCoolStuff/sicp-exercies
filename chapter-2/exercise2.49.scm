a62dbb3589dcf158424f80e321cebe86e13c9e37
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-06-09 00:48:33 -0400

    add exercise 2.49

---
+++ chapter-2/exercise2.49.scm
@@ -0,0 +1,93 @@
+; a
+(segments->painter
+  (list
+    (make-segment
+      (make-vect 0 0)
+      (make-vect 1 0))
+    (make-segment
+      (make-vect 1 0)
+      (make-vect 1 1))
+    (make-segment
+      (make-vect 1 1)
+      (make-vect 0 1))
+    (make-segment
+      (make-vect 0 1)
+      (make-vect 0 0))))
+; b
+(segments->painter
+  (list
+    (make-segment
+      (make-vect 0 0)
+      (make-vect 1 1))
+    (make-segment
+      (make-vect 0 1)
+      (make-vect 1 0))))
+; c
+(segments->painter
+  (list
+    (make-segment
+      (make-vect 0.5 0)
+      (make-vect 0 0.5))
+    (make-segment
+      (make-vect 0 0.5)
+      (make-vect 0.5 1))
+    (make-segment
+      (make-vect 0.5 1)
+      (make-vect 1 0.5))
+    (make-segment
+      (make-vect 1 0.5)
+      (make-vect 0.5 0))))
+; d
+(segments->painter
+  (list
+    (make-segment
+      (make-vect 0.15 0)
+      (make-vect 0.39 0.47))
+    (make-segment
+      (make-vect 0.39 0.47)
+      (make-vect 0.3  0.62))
+    (make-segment
+      (make-vect 0.3  0.62)
+      (make-vect 0.11 0.45))
+    (make-segment
+      (make-vect 0.11 0.45)
+      (make-vect 0    0.64))
+    (make-segment
+      (make-vect 0 0.75)
+      (make-vect 0.15 0.62))
+    (make-segment
+      (make-vect 0.15 0.62)
+      (make-vect 0.33 0.7))
+    (make-segment
+      (make-vect 0.33 0.7)
+      (make-vect 0.43 0.7))
+    (make-segment
+      (make-vect 0.43 0.7)
+      (make-vect 0.33 0.86))
+    (make-segment
+      (make-vect 0.33 0.86)
+      (make-vect 0.44 1))
+    (make-segment
+      (make-vect 0.59 1)
+      (make-vect 0.69 0.84))
+    (make-segment
+      (make-vect 0.69 0.84)
+      (make-vect 0.62 0.7))
+    (make-segment
+      (make-vect 0.62 0.7)
+      (make-vect 0.77 0.69))
+    (make-segment
+      (make-vect 0.77 0.69)
+      (make-vect 1 0.47))
+    (make-segment
+      (make-vect 1 0.2)
+      (make-vect 0.65 0.42))
+    (make-segment
+      (make-vect 0.65 0.42)
+      (make-vect 0.8 0))
+    (make-segment
+      (make-vect 0.7 0)
+      (make-vect 0.48 0.3))
+    (make-segment
+      (make-vect 0.48 0.3)
+      (make-vect 0.32 0))))
