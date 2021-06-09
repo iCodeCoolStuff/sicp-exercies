e2b37640c79148c4dadd6bb91bc6092181c14d74
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-06-08 23:00:26 -0400

    fix

--- chapter-2/exercise2.48.scm
+++ chapter-2/exercise2.48.scm
@@ -1,5 +1,5 @@
 (define (make-line-segment start-vector end-vector)
-  (cons start-vector end-vector))
+  (cons start-vector (cons end-vector ())))
 
 (define start-segment car)
 (define end-segment cdr)
