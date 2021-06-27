ec09fe10a36ea6699ddb6b49bbd3749852e36b84
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-06-26 21:49:35 -0400

    fix exercise 2.91

--- chapter-2/exercise2.91.scm
+++ chapter-2/exercise2.91.scm
@@ -7,12 +7,11 @@
 	    (list (the-empty-termlist) L1)
 	    (let ((new-c (div (coeff t1) (coeff t2)))
 		  (new-o (- (order t1) (order t2))))
-	      (let ((new-term (make-term new-c new-o))
-		    (rest-of-result 
-		      (div-terms (sub-terms L1 (mul-term-by-all-terms new-term L1)) L2)))
+	      (let ((rest-of-result 
+		      (div-terms (sub-terms L1 (mul-term-by-all-terms (make-term new-o new-c)  L2)) L2)))
 		(let ((terms     (car rest-of-result))
 		      (rem       (cdr rest-of-result)))
-		  (list (adjoin-term new-term terms) rem))))))))
+		  (list (adjoin-term (make-term new-o new-c) terms) rem))))))))
 
 (define (div-poly p1 p2)
   (if (same-variable? p1 p2)
