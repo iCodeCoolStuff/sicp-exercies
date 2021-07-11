9ec4577b3bd9fdebef8b3d6d37456db64747a2eb
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-07-10 22:27:28 -0400

    add exercise 3.14

---
+++ chapter-3/exercise3.14.scm
@@ -0,0 +1,11 @@
+; Mystery reverses x. 
+;
+; v-> [o][o]-->[o][o]-->[o][/]
+;      |        |        |
+;      V        V        V
+;     'c       'b       'a
+;
+; w-> [o][o]-->[o][o]-->[o][/]
+;      |        |        |
+;      V        V        V
+;     'a       'b       'c
