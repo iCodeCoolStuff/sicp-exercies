05636955b66d7fab09c28a6ea43f6a280a387847
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-07-30 17:51:20 -0400

    add exercise 3.27

--- chapter-3/exercise3.27.scm
+++ chapter-3/exercise3.27.scm
@@ -39,3 +39,28 @@
       (cond ((= n 0) 0)
             ((= n 1) 1)
 	    (else (+ (memo-fib (- n 1)) (memo-fib (- n 2))))))))
+
+; Environment Diagram: 
+; 
+;             +-------------+
+; global env: | memo-fib:-+ |
+;             +-----------|-+
+;             E1:         V
+; +------+    +-------------+
+; |get:3 |--->| memo-fib: 3 |<---- +
+; +------+    +-----------|-+      |
+;             E3:         V        | E2:
+; +------+    +-------------+    +-------------+   +------+
+; |get:2 |--->| memo-fib: 2 |    | memo-fib: 1 |<--|get:1 |
+; +------+    +-----------|-+    +-------------+   +------+
+;             E4:         V
+; +------+    +-------------+
+; |get:1 |--->| memo-fib: 1 |
+; +------+    +-------------+
+
+; memo-fib computes the number of steps in proportion to n because the first recursive call in memo-fib always
+; decreases by 1 which decreases with respect to n. The second call in memo-fib is already memoized,
+; so the second call will always run in O(1) time. This results in an O(2n-1) runtie or O(n) runtime.
+
+; The scheme would not work if we did (memoize fib) because the memoize function calls the unmemoized
+; version of the fib function. This is why we need to define memo-fib in terms of itself.
