d470de3d5feb7d1dc485d3d3107a654347d9521f
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-06 23:18:00 -0400

    add exercise 3.41

---
+++ chapter-3/exercise3.41.scm
@@ -0,0 +1,3 @@
+; I don't agree. The only scenario I could think of is where a user tries to withdraw more money than the bank
+; account has available due to a false number displayed by the bank balance. However, the user could simply
+; query the bank account again to resolve the issue.
