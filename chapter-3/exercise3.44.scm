e7a1dce7734b8b6dce30c665eb7d619198dae4fb
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-07 22:41:27 -0400

    add exercise 3.44

---
+++ chapter-3/exercise3.44.scm
@@ -0,0 +1,4 @@
+; Louis Reasoner is incorrect. The essential difference between the exchange problem and the transfer problem is that
+; the concurrency of the exchange problem caused exchange to operate on past not current values of the accounts. This
+; does not exist in the transfer problem because transfer only is concerned with whether or not the account withdrawn
+; from has enough funds. Any error in the transfer system is purely human and not caused by a computer.
