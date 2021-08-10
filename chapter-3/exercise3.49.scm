db1487e5304ab074013fa10cca9de9471b5cf9ab
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-10 15:15:51 -0400

    add exercise 3.49

---
+++ chapter-3/exercise3.49.scm
@@ -0,0 +1,2 @@
+; Let's say you have a concurrent tree sorting algorithm with two workers. One worker grabs onto one node while another worker
+; grabs onto another node. A deadlock would occur if both workers needed to access the node that the other worker acquired.
