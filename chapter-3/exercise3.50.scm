b2af4ad299f75093cacf41269c835968ac896739
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-11 11:44:44 -0400

    fix exercise 3.50

--- chapter-3/exercise3.50.scm
+++ chapter-3/exercise3.50.scm
@@ -3,11 +3,11 @@
   (if (= n 0)
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
-;(define (stream-map proc s)
-  ;(if (stream-null? s)
-      ;'done
-      ;(begin (proc (stream-car s))
-	     ;(stream-for-each proc (stream-cdr s)))))
+(define (stream-map proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+	     (stream-for-each proc (stream-cdr s)))))
 (define (stream-for-each proc s)
   (if (stream-null? s)
       'done
@@ -38,17 +38,19 @@
     (lambda ()
       (if (not already-run?)
 	  (begin (set! result (proc))
-	         (set! alread-run? true)
+	         (set! already-run? true)
 		 result)
 	  result))))
 (define (delay exp)
   (memo-proc (lambda () exp)))
+(define (cons-stream a b)
+  (cons a (delay b)))
 
 
 (define (stream-map proc . argstreams)
   (if (null? (car argstreams))
       the-empty-stream
-      (cons
-	(apply proc (map car argstreams))
+      (cons-stream
+	(apply proc (map stream-car argstreams))
 	(apply stream-map
 	       (cons proc (map cdr argstreams))))))
