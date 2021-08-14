81382bdd2e4336e636e085562920804a1d041e8a
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-14 16:34:36 -0400

    add exercise 3.57

---
+++ chapter-3/exercise3.57.scm
@@ -0,0 +1,4 @@
+; The number of additions performed when comuting the nth fibonacci number is equal to n. However, the number of additions
+; to compute the nth fibonacci number without the memo-proc optimization is equal to phi^n where phi is equal to the golden ratio.
+; The reason for this is because the ratio of the number of additions required to compute the nth fibonacci number divided by
+; the number of additions required to compute the n-1th fibonacci number approaches phi.
