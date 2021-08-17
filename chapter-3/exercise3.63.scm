fdc0b3c4c5423299d555104a7d0eb8cd84e61bec
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-16 21:36:57 -0400

    add exercise 3.63

--- chapter-3/exercise3.63.scm
+++ chapter-3/exercise3.63.scm
@@ -14,4 +14,7 @@
 
 ; Louis's version of sqrt-stream has to compute the last guesses every time it wants to process a new guess. This is because
 ; sqrt-stream generates a new stream-map for each guess. The guesses version of sqrt-stream doesn't have this problem because
-; stream-map always takes in a new instance of guesses instead of building on top of all the old stream-maps.
+; stream-map always uses a new instance of guesses instead of building on top of all the old stream-maps.
+
+; The two versions would not differ in efficiency after removing the memo because now both versions have to calculate the result
+; of (sqrt-improve guess x) again.
