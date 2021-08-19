06e4b5107c06df9ac5eb5bcf5e2285199849f279
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-19 14:23:51 -0400

    better

--- chapter-3/exercise3.70.scm
+++ chapter-3/exercise3.70.scm
@@ -16,26 +16,6 @@
   (cons-stream n (integers-starting-from (+ n 1))))
 (define integers (integers-starting-from 1))
 
-(define (merge s1 s2)
-  (cond ((stream-null? s1) s2)
-        ((stream-null? s2) s1)
-	(else
-	  (let ((s1car (stream-car s1))
-		(s2car (stream-car s2)))
-	    (cond ((< s1car s2car)
-		   (cons-stream
-		     s2car
-		     (merge (stream-cdr s1) s2)))
-	          ((> s1car s2car)
-		   (cons-stream
-		     s1car
-		     (merge s1 (stream-cdr s2))))
-		  (else
-		    (cons-stream
-		      s1car
-		        (merge (stream-cdr s1)
-			       (stream-cdr s2)))))))))
-
 (define (merge-weighted s1 s2 weight)
   (cond ((stream-null? s1) s2)
         ((stream-null? s2) s1)
@@ -44,17 +24,27 @@
 		(s2car (stream-car s2)))
 	    (cond ((< (weight s1car) (weight s2car))
 		   (cons-stream
-		     s2car
+		     s1car
 		     (merge-weighted (stream-cdr s1) s2 weight)))
 	          ((> (weight s1car) (weight s2car))
 		   (cons-stream
-		     s1car
+		     s2car
 		     (merge-weighted s1 (stream-cdr s2) weight)))
 		  (else
 		    (cons-stream
 		      s1car
-		      (merge-weighted (stream-cdr s1)
-			     (stream-cdr s2) weight))))))))
+		      (cons-stream
+			s2car
+		        (merge-weighted (stream-cdr s1)
+			       (stream-cdr s2) weight)))))))))
+
+(define (weighted-pairs s t weight)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (merge-weighted
+      (stream-map (lambda (x) (list (stream-car s) x))
+		  (stream-cdr t))
+      (weighted-pairs (stream-cdr s) (stream-cdr t) weight) weight)))
 
 (define (display-line x) (newline) (display x))
 (define (display-stream s)
@@ -65,14 +55,16 @@
     (+ (car p) (cadr p)))
   (define (proc2 p)
     (+ (* 2 (car p)) (* 3 (cadr p)) (* 5 (car p) (cadr p))))
-  (define s (merge-weighted (pairs integers integers) (pairs integers integers) proc))
+
+
+  (define s (weighted-pairs integers integers proc))
   (define (divisible? x y) (= (remainder x y) 0))
   (define (not-divisible? p) (not (or (divisible? (car p) 2) (divisible? (car p) 3) (divisible? (car p) 5)
 				      (divisible? (cadr p) 2) (divisible? (cadr p) 3) (divisible? (cadr p) 5))))
-  (define s2 (stream-filter not-divisible? (merge-weighted (pairs integers integers) (pairs integers integers) proc2)))
+  (define s2 (stream-filter not-divisible? (weighted-pairs integers integers proc2)))
 
   (define (stream-iter s)
-    (if (not (and (= (car (stream-car s)) 1) (= (cadr (stream-car s)) 100)))
+    (if (not (and (= (car (stream-car s)) 13) (= (cadr (stream-car s)) 17)))
 	(begin (display-line (stream-car s))
 	       (stream-iter (stream-cdr s)))))
   (stream-iter s2))
