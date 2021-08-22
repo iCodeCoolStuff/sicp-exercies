cc2e97cdde7e90e5ad11df85d931d5f9fe8d9028
icodecoolstuff <rodriguezisaac799@gmail.com>
2021-08-22 11:07:38 -0400

    better

--- chapter-3/exercise3.82.scm
+++ chapter-3/exercise3.82.scm
@@ -20,21 +20,15 @@
     (f (stream-car s) (stream-car (stream-cdr s)))
     (map-successive-pairs f (stream-cdr (stream-cdr s)))))
 
-(define (interleave s1 s2)
-  (cons-stream
-    (stream-car s1)
-    (interleave s2 (stream-cdr s1))))
-
 (define (estimate-integral pred x1 x2 y1 y2)
-  (define (random-x-stream n)
-    (cons-stream n (random-x-stream (random-in-range x1 x2))))
-  (define (random-y-stream n)
-    (cons-stream n (random-y-stream (random-in-range y1 y2))))
-  (define random-pairs
-    (interleave (random-x-stream (random-in-range x1 x2))
-		(random-y-stream (random-in-range y1 y2))))
+  (define (make-random-pairs)
+    (cons-stream
+      (random-in-range x1 x2)
+      (cons-stream
+	(random-in-range y1 y2)
+	(make-random-pairs))))
   (define integral-experiment
-    (map-successive-pairs pred random-pairs))
+    (map-successive-pairs pred (make-random-pairs)))
   (stream-map (lambda (n) (* n (area x1 x2 y1 y2))) (monte-carlo integral-experiment 0 0)))
 
 (define pi-estimate
