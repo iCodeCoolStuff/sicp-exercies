53f7a22c242fd6ba971c3f3c2a15905152910bc2
isaac <rodriguezisaac799@gmail.com>
2021-08-24 21:06:34 -0400

    add envs

--- chapter-4/exercise4.4.scm
+++ chapter-4/exercise4.4.scm
@@ -1,17 +1,17 @@
 (define (and-exps exp) (cdr exp))
 (define (or-exps exp) (cdr exp))
 
-(define (eval-and exp)
+(define (eval-and exp env)
   (define (and-rec exps)
     (cond ((null? exps) true)
-	  ((not (eval (car exps))) false)
+	  ((not (eval (car exps) env)) false)
 	  (else (and-rec (cdr exps)))))
   (and-rec (and-exps exp)))
 	   
-(define (eval-or exp)
+(define (eval-or exp env)
   (define (or-rec exps)
     (cond ((null? exps) false)
-	  ((eval (car exps)) true)
+	  ((eval (car exps) env) true)
 	  (else (or-rec (cdr exps)))))
   (or-rec (or-exps exp)))
 
