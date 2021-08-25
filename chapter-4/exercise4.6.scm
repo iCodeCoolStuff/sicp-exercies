41403f707f51059fba58dccc53596485ac2fdf13
isaac <rodriguezisaac799@gmail.com>
2021-08-24 21:06:53 -0400

    add exercise 4.6

---
+++ chapter-4/exercise4.6.scm
@@ -0,0 +1,10 @@
+(define (let? exp) (tagged-list? exp 'let))
+(define (let-exps exp) (cadr exp))
+(define (let-variables exp) (map car (let-exps exp)))
+(define (let-body exp) (caddr exp))
+
+(define (let->combination exp)
+  (list (make-lambda (let-variables exp) (let-body exp)) (let-exps exp))) 
+
+(define (eval-let exp env)
+  (eval (let-combination exp) env))
