b67ce0c4bb96beb2013133df4a20d09a2735cdca
isaac <rodriguezisaac799@gmail.com>
2021-09-03 14:23:16 -0400

    add exercise 4.7

--- chapter-4/exercise4.7.scm
+++ chapter-4/exercise4.7.scm
@@ -1,20 +1,24 @@
+(define (let*? exp) (tagged-list? exp 'let*))
 (define (let*-clauses exp) (cadr exp))
-(define (let*-body exp) (caddr exp))
-(define (let*->nested-lets exp)
-  (expand-lets (let*-clauses exp) (let*-body exp)))
+(define (let*-body exp) (cddr exp))
 (define (variable clause)
   (car clause))
 (define (expression clause)
   (cadr clause))
+(define (let*->nested-lets exp)
+  (let* ((clauses (let*-clauses exp))
+	 (first (car clauses))
+	 (rest (cdr clauses)))
+    (make-let (list first)
+	      (expand-lets (let*-clauses exp) (let*-body exp)))))
 
 (define (expand-lets clauses body)
-  (let ((first (car clauses))
-	(rest  (cdr clauses)))
-    (if (null? rest)
-	body
-	(make-lambda (list (variable first))
-		     (expression first)
-		     (expand-lets rest body)))))
+  (if (null? clauses)
+      body
+      (list (make-let (list (car clauses))
+		(expand-lets (cdr clauses) body)))))
 
 (define (eval-let* exp env)
   (eval (let*->nested-lets exp) env))
+
+; It is possible to write let* in terms of derived expressions.
