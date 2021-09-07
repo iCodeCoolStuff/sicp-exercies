a0075a2e6bcb40a189c02a115813ecbd0b2ed991
isaac <rodriguezisaac799@gmail.com>
2021-09-07 14:27:38 -0400

    works

--- chapter-4/exercise4.9.scm
+++ chapter-4/exercise4.9.scm
@@ -4,98 +4,88 @@
 (define (loop-return exp) (cadddr exp))
 
 (define (make-loop condition body return)
-  (list condition body return))
-(define (make-definition var value)
-  (list var value))
+  (list 'loop condition body return))
 
 (define (make-definition def-variable def-parameters def-body)
   (list 'define def-variable (make-lambda def-parameters def-body)))
 
 (define (loop->application exp)
-  (make-begin
-    (list (make-definiton 'loop '() 
-			  (make-begin (list (make-if (loop-condition exp)
-			      	                     (loop-body exp)
-				                     (loop-return exp)))))
-	  '(loop))))
+  (sequence->exp
+    (list (make-definition 'loop-inner '() 
+			  (list (make-if (loop-condition exp)
+			      	                     (sequence->exp
+						       (list (loop-body exp) 
+						       '(loop-inner)))
+				                     (loop-return exp))))
+	  '(loop-inner))))
 
 (define (eval-loop exp env)
   (eval (loop->application exp) env))
 
-;(define (eval-loop exp env)
-  ;(if (eval (loop-condition exp) env)
-      ;(begin (eval (loop-body exp) env)
-	     ;(eval-loop exp))
-      ;(loop-return exp)))
-
 (define (while? exp) (tagged-list? exp 'while))
 (define (while-condition exp) (cadr exp))
-(define (while-body exp) (caddr exp))
+(define (while-body exp) (cddr exp))
 (define (make-while condition body)
   (list 'while condition body))
 
 (define (while->loop exp)
-  (make-loop (while-condition exp) (while-body exp) '()))
+  (make-loop (while-condition exp) (sequence->exp (while-body exp)) ''done))
 
 (define (make-not exp) (list 'not exp))
-(define (not-contents not-exp) (cadr exp))
+(define (not-contents exp) (cadr exp))
 (define (eval-not exp env)
-  (if (eq? (eval (not-contents exp) env) 'false)
-      'true
-      'false))
+  (if (true? (eval (not-contents exp) env))
+      false
+      true))
 
+(define (not? exp) (tagged-list? exp 'not))
 (define (until? exp) (tagged-list? exp 'until))
 (define (until-condition exp) (cadr exp))
-(define (until-body exp) (caddr exp))
+(define (until-body exp) (cddr exp))
 (define (make-until condition body)
   (list 'until condition body))
 (define (until->loop exp)
-  (make-loop (make-not (until-condition exp)) (until-body exp) '()))
+  (make-loop (make-not (until-condition exp)) (sequence->exp (until-body exp)) ''done))
 
-(define (make-let-clause var exp))
+(define (make-let-clause var exp) (list var exp))
 (define (make-let clauses body)
   (list 'let clauses body))
 (define (for-var exp) (cadr exp))
 (define (for-list exp) (cadddr exp))
-(define (for-body exp) (fourth exp))
-(define (for? exp)
-  (and
-    (tagged-list? exp 'for)
-    (symbol? (for-var exp))
-    (eq? (caddr exp) 'in)
-    (list? (for-list exp))))
+(define (for-body exp) (cddddr exp))
+(define (for? exp) (tagged-list? exp 'for))
 
 (define (make-for variable lst body)
   (list 'for variable 'in lst body))
-(define (for->let exp)
-  (expand-for (for-var exp) (for-list exp) (for-body exp)))
+
+(define (eval-for exp env)
+  (let ((f-list (eval (for-list exp) env)))
+    (eval (expand-for (for-var exp) f-list (sequence->exp (for-body exp))) env)))
 
 (define (expand-for var lst body)
   (if (null? lst)
-      '()
+      ''done
       (make-let (list (make-let-clause var (car lst)))
-		(make-begin 
-		  (list body (expand-for var (cdr lst) body))))))
+		(list body (expand-for var (cdr lst) body)))))
 
 (define (do? exp) (tagged-list? exp 'do))
 (define (do-bindings exp) (cadr exp))
 (define (variable-bindings exp) (map (lambda (b) (list (car b) (cadr b))) (do-bindings exp)))
 (define (do-variables exp) (map car (do-bindings exp)))
 (define (do-inits exp) (map cadr (do-bindings exp)))
-(define (do-steps exp) (map cddr (do-bindings exp)))
-(define (do-test exp) (caddr exp))
+(define (do-steps exp) (map caddr (do-bindings exp)))
+(define (do-test exp) (caaddr exp))
 (define (do-return exp) (cdaddr exp))
-(define (do-body exp) (cadddr exp))
+(define (do-body exp) (cdddr exp))
 
-(define (do->let->application exp)
-  (make-begin
-    (list (make-definition 'do
-          (do-variables exp) (make-if (do-test exp)
-				      (do-return exp)
-				      (make-begin
-				        (list (do-body exp)
-					      (cons 'do (do-steps exp))))))
-	  (cons 'do (do-inits exp)))))
+(define (do->let->exp exp)
+  (sequence->exp
+    (list (make-definition 'do-inner
+          (do-variables exp) (list (make-if (do-test exp)
+				      (sequence->exp (do-return exp))
+				      (sequence->exp (list (sequence->exp (do-body exp))
+					                   (cons 'do-inner (do-steps exp)))))))
+	  (cons 'do-inner (do-inits exp)))))
 
 ; Iteration constructs: loop, while, until, for, do.
 ;
@@ -148,7 +138,7 @@
 ;  (display (square i)))
 ;
 ; Do is useful for functional programming (https://groups.csail.mit.edu/mac/ftpdir/scheme-mail/HTML/rrrs-1986/msg00300.html).
-; You might be unfamiliar with do, so here is a quick guide. Do is the form (do ((variable init step) ...) (test expression ...) command ...)
+; Do is the form (do ((variable init step) ...) (test expression ...) command ...)
 ;
 ; Usage:
 ;
