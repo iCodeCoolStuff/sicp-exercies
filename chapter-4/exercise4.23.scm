6e2fce780453446f1f7700306a3fab635e791167
isaac <rodriguezisaac799@gmail.com>
2021-09-10 22:41:08 -0400

    add exercise 4.23

---
+++ chapter-4/exercise4.23.scm
@@ -0,0 +1,7 @@
+; 1. Alyssa's program will analyze the procedure body and then execute each proc during analyze-sequence.
+
+; 2. The execution procedure produced by the text above will produce an analyzed sequence and save it
+;    for when it comes time to execute it.
+
+; The version in the text above will be faster because there won't be repeated analysis of whether
+; or not there are more procs in the sequence of expressions.
