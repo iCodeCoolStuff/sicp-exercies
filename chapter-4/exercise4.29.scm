7cae5809d66f4e732ab934a8e87681927e835027
isaac <rodriguezisaac799@gmail.com>
2021-09-12 20:23:30 -0400

    add exercise 4.29

---
+++ chapter-4/exercise4.29.scm
@@ -0,0 +1,31 @@
+; Classic example. I'm sure there are plenty of others.
+(define (fib n)
+  (cond ((= n 0) 0)
+	((= n 1) 1)
+	(else (+ (fib (- n 1)) (fib (- n 2))))))
+
+; With memoization
+
+(define (square x)
+  (* x x))
+;;; L-Eval input:
+(square (id 10))
+;;; L-Eval value:
+100
+;;; L-Eval input:
+count
+;;; L-Eval value:
+1
+
+; Without memoization
+
+(define (square x)
+  (* x x))
+;;; L-Eval input:
+(square (id 10))
+;;; L-Eval value:
+100
+;;; L-Eval input:
+count
+;;; L-Eval value:
+2
