1a017c3e955c926779bb61c9e7eb56f9be8ed15b
isaac <rodriguezisaac799@gmail.com>
2021-09-13 15:40:29 -0400

    force every value except for the last one

--- chapter-4/leval.scm
+++ chapter-4/leval.scm
@@ -90,7 +90,7 @@
 
 (define (eval-sequence exps env)
   (cond ((last-exp? exps) (eval (first-exp exps) env))
-	(else (eval (first-exp exps) env)
+	(else (actual-value (first-exp exps) env)
 	      (eval-sequence (rest-exps exps) env))))
 
 (define (eval-sequences exps env)
