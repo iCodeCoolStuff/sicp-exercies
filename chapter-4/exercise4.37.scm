af42fa86336e95afe7bbf94b291723b4643e474a
isaac <rodriguezisaac799@gmail.com>
2021-10-08 06:49:39 -0400

    add exercise 4.37

---
+++ chapter-4/exercise4.37.scm
@@ -0,0 +1,21 @@
+(define (require p)
+  (if (not p) (amb)))
+
+(define (an-integer-between low high)
+  (require (<= low high))
+  (amb low (an-integer-between (+ low 1) high)))
+
+(define (an-integer-starting-from n)
+  (amb n (an-integer-starting-from (+ n 1))))
+
+(define (a-pythagorean-triple-between low high)
+  (let ((i (an-integer-between low high))
+        (hsq (* high high)))
+    (let ((j (an-integer-between i high)))
+      (let ((ksq (+ (* i i) (* j j))))
+        (require (>= hsq ksq))
+        (let ((k (sqrt ksq)))
+          (require (integer? k))
+          (list i j k))))))
+
+; Yes. At the very least, Ben's version does not have to test numbers whose sum of squares is greater than the highest possible square (* high high).
