8f200b28b95219693f1cdac1ee9be0dff1c795d5
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-05-25 12:58:42 -0400

    add exercise 4.49

---
+++ chapter-4/exercise4.49.scm
@@ -0,0 +1,81 @@
+(define nouns '(noun student professor cat class))
+(define verbs '(verb studies lectures eats sleeps))
+(define articles '(article the a))
+(define adjectives '(adjective red orange yellow green blue purple))
+(define prepositions '(prep for to in by with))
+(define adverbs      '(adverb slowly quickly steadily terribly))
+(define conjunctions '(conj for and nor but or yet so))
+
+(define *unparsed* '())
+(define (parse input)
+  (set! *unparsed* input)
+  (let ((sent (parse-sentence)))
+    (require (null? *unparsed*))
+    sent))
+
+(define (parse-sentence)
+  (define (maybe-extend sentence)
+    (amb sentence
+	 (append sentence
+		 (list (parse-word conjunctions))
+		 (list (parse-noun-phrase))
+		 (parse-verb-phrase))))
+  (maybe-extend
+    (list 'sentence
+          (parse-noun-phrase)
+	  (parse-verb-phrase))))
+
+(define (parse-simple-noun-phrase)
+  (append (list 'simple-noun-phrase
+                (parse-word articles))
+                (maybe-parse-adjective-list)))
+
+(define (maybe-parse-adjective-list)
+  (define (maybe-add-adjective adjective-phrase)
+    (amb (append adjective-phrase
+		 (list (parse-word nouns)))
+	 (maybe-add-adjective (append adjective-phrase
+		                      (list (parse-word adjectives))))))
+  (amb (list (parse-word nouns))
+       (maybe-add-adjective (list (parse-word adjectives)))))
+	      
+(define (parse-verb-phrase)
+  (define (maybe-append-adverb verb-list)
+    (amb (maybe-append-adverb (append verb-list (list (parse-word adverbs))))
+	 verb-list))
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+	 (maybe-extend (append verb-phrase
+			       (list (parse-prepositional-phrase))))))
+  (maybe-extend (maybe-append-adverb (list 'verb-phrase (parse-word verbs)))))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+	 (maybe-extend (list 'noun-phrase
+			     noun-phrase
+			     (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+	(parse-word prepositions)
+	(parse-noun-phrase)))
+
+(define (parse-word word-list)
+  (require (not (null? *unparsed*)))
+  (require (memq (car *unparsed*) (cdr word-list)))
+  (let ((found-word (list-ref (cdr word-list) (random-integer (length (cdr word-list))))))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
+
+; a cat eats in a cat to the class
+; a cat eats in a cat with a professor
+; a red student lectures by the professor
+; the student lectures nor a student lectures
+; a red professor lectures with the class with a orange class
+; the student studies steadily
+; a orange student studies in the student nor a class lectures steadily with the professor
+; the class studies steadily with a professor
+; the student sleeps steadily by a cat in the class
+; the green cat sleeps for a green class
