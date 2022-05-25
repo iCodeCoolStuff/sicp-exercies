d42cff0ec1c4d2b386620cb9b1b40df697ce1225
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-05-25 16:06:39 -0400

    add ramb and random selection to parser

--- chapter-4/exercise4.50.scm
+++ chapter-4/exercise4.50.scm
@@ -600,3 +600,75 @@
          (lambda (val next-alternative) val)
          (lambda () 'fail))
 (driver-loop)
+
+(define nouns '(noun student professor cat class))
+(define verbs '(verb studies lectures eats sleeps))
+(define articles '(article the a))
+(define adjectives '(adjective red orange yellow green blue purple))
+(define prepositions '(prep for to in by with))
+(define adverbs      '(adverb slowly quickly steadily terribly))
+(define conjunctions '(conj for and nor but or yet so))
+
+(define *unparsed* '())
+(define (parse input)
+  (set! *unparsed* input)
+  (let ((sent (parse-sentence)))
+    (require (null? *unparsed*))
+    sent))
+
+(define (parse-sentence)
+  (define (maybe-extend sentence)
+    (ramb sentence
+	 (append sentence
+		 (list (parse-word conjunctions))
+		 (list (parse-noun-phrase))
+		 (parse-verb-phrase))))
+  (maybe-extend
+    (list 'sentence
+          (parse-noun-phrase)
+          (parse-verb-phrase))))
+
+(define (parse-simple-noun-phrase)
+  (append (list 'simple-noun-phrase
+                (parse-word articles))
+                (maybe-parse-adjective-list)))
+
+(define (maybe-parse-adjective-list)
+  (define (maybe-add-adjective adjective-phrase)
+    (ramb (append adjective-phrase
+		 (list (parse-word nouns)))
+	 (maybe-add-adjective (append adjective-phrase
+		                      (list (parse-word adjectives))))))
+  (ramb (list (parse-word nouns))
+       (maybe-add-adjective (list (parse-word adjectives)))))
+	      
+(define (parse-verb-phrase)
+  (define (maybe-append-adverb verb-list)
+    (ramb (maybe-append-adverb (append verb-list (list (parse-word adverbs))))
+	 verb-list))
+  (define (maybe-extend verb-phrase)
+    (ramb verb-phrase
+	 (maybe-extend (append verb-phrase
+			       (list (parse-prepositional-phrase))))))
+  (maybe-extend (maybe-append-adverb (list 'verb-phrase (parse-word verbs)))))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (ramb noun-phrase
+	 (maybe-extend (list 'noun-phrase
+			     noun-phrase
+			     (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+	(parse-word prepositions)
+	(parse-noun-phrase)))
+
+(define (parse-word word-list)
+  (require (not (null? *unparsed*)))
+  (define (a-random-word-from wl)
+    (list-ref (cdr wl) (random-integer (length (cdr wl)))))
+  (let ((found-word (a-random-word-from word-list)))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
