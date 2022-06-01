42c43b59ce3c80469e02e15aa79357d005044d6b
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-06-01 00:19:49 -0400

    correct answer

--- chapter-4/exercise4.60.scm
+++ chapter-4/exercise4.60.scm
@@ -1,13 +1,3 @@
-; This happens because the query system looks for *all* variables ?person1 and ?person2 that satisfy lives-near.
-; There is a way to find a list of people who live near each other in which pair appears only once, but the naive approach does not work. An example of a failed query is below.
-
-(and (lives-near ?person1 ?person2)
-     (not (lives-near ?person2 ?person1)))
-
-; The above query does not work because it rules out all people who live near each other regardless of the order of the persons. The query ends up returning nothing.
-; However, the below query does work.
-
-(and (lives-near ?person1 ?person2)
-     (not (lives-near ?person2 ?_)))
-
-; This query rules out all possibilities where person2 is first. Therefore, no queries where both person1 and person2 are listed twice will appear.
+; This happens because the query system looks for *all* variables ?person1 and ?person2 that satisfy lives-near. Within the query language, this is impossible. not filters out symmetric queries.
+; Tbe only way to do this is to either use a special form or a hack with lisp-value that filters
+; out data based on a certain characteristic of that data. The latter is not a robust solution.
