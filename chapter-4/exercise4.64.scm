d3cd120ba7199ea0980531f8efdfe83cc236a111
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-06-01 00:35:02 -0400

    add exercise 4.64

---
+++ chapter-4/exercise4.64.scm
@@ -0,0 +1,2 @@
+; The system goes into an infinite loop because the query system will attempt to unify (outranked-by ?middle-manager ?boss) with the conclusion and the previous or query. It expands
+; the outranked-by query indefinitely because the system can come up with a binding for ?middle-manager that always fails the first or clause.
