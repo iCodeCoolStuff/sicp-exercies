49293d2a08a6a14460424cd35bfe9107ab6d3d35
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-06-01 00:37:06 -0400

    add exercise 4.65

---
+++ chapter-4/exercise4.65.scm
@@ -0,0 +1,3 @@
+; Warbucks Oliver is displayed 4 times because there are several bindings that satisfy
+; the wheel query when Warbucks Oliver is the wheel. There is also no query to filter
+; out duplicates in the output stream.
