ec60000d4a624fb984f8ca1a929ecabbe32406b4
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-06-08 13:24:45 -0400

    add exercise 4.74

---
+++ chapter-4/exercise4.74.scm
@@ -0,0 +1,9 @@
+; a)
+(define (simple-stream-flatmap proc s)
+  (simple-flatten (stream-map proc s)))
+
+(define (simple-flatten stream)
+  (stream-map stream-car
+              (stream-filter (lambda (s) (not (stream-null? s))) stream)))
+; b)
+No. The behavior of the program will not change.
