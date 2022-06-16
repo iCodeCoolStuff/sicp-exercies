587db25b520c91e3617b687fae53fe92ef2de32f
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-06-15 20:04:30 -0400

    add exercise 5.2

---
+++ chapter-5/exercise5.2.scm
@@ -0,0 +1,27 @@
+(data-paths
+ (registers
+  ((name product)
+   (buttons ((name t1<-product) (source (operation *)))))
+  ((name counter)
+   (buttons ((name t2<-counter) (source (operation +)))))
+  ((name t1)
+   (buttons ((name product<-t1) (source (register t1)))))
+  ((name t2)
+   (buttons ((name counter<-t2) (source (register t2))))))
+
+ (operations
+  ((name *)
+   (inputs (register product) (register counter)))
+  ((name +)
+   (inputs (register counter) (constant 1)))))
+
+(controller
+ test-gt
+   (test >)
+   (branch (label factorial-done))
+   (t1<-product)
+   (product<-t1)
+   (t2<-counter)
+   (counter<-t2)
+   (goto (label test-gt))
+ factorial-done)
