6923eb191983b8d437e01ebf20ee3e01ac2326e8
iCodeCoolStuff <rodriguezisaac799@gmail.com>
2022-07-14 15:02:01 -0400

    add cond

--- chapter-5/ch5-eceval.scm
+++ chapter-5/ch5-eceval.scm
@@ -22,7 +22,6 @@
 ;; print-result in the machine controller below
 ;;**Also choose the desired make-stack version in regsim.scm
 
-
 (define eceval-operations
   (list
    ;;primitive Scheme operations
@@ -44,7 +43,10 @@
    (list 'lambda-body lambda-body)
    (list 'if? if?)
    (list 'cond? cond?)
-   (list 'cond->if cond->if)
+   (list 'cond-clauses cond-clauses)
+   (list 'cond-predicate cond-predicate)
+   (list 'cond-else-clause? cond-else-clause?)
+   (list 'cond-actions cond-actions)
    (list 'if-predicate if-predicate)
    (list 'if-consequent if-consequent)
    (list 'if-alternative if-alternative)
@@ -59,6 +61,8 @@
    (list 'no-operands? no-operands?)
    (list 'first-operand first-operand)
    (list 'rest-operands rest-operands)
+   (list '+ +)
+   (list '= =)
 
    ;;operations in eceval-support.scm
    (list 'true? true?)
@@ -264,9 +268,34 @@ ev-if-alternative
 ev-if-consequent
   (assign exp (op if-consequent) (reg exp))
   (goto (label eval-dispatch))
+
 ev-cond
-  (assign exp (op cond->if) (reg exp))
-  (goto (label ev-if))
+  (assign unev (op cond-clauses) (reg exp))
+  (save continue)
+  (goto (label ev-clauses))
+ev-clauses
+  (assign exp (op first-exp) (reg unev))
+  (test (op cond-else-clause?) (reg exp))
+  (branch (label ev-cond-else))
+  (save exp)
+  (save env)
+  (save unev)
+  (assign exp (op cond-predicate) (reg exp))
+  (assign continue (label ev-cond-pred))
+  (goto (label eval-dispatch))
+ev-cond-pred
+  (restore unev)
+  (restore env)
+  (restore exp)
+  (test (op true?) (reg val))
+  (branch (label ev-cond-result))
+  (assign unev (op rest-exps) (reg unev))
+  (goto (label ev-clauses))
+ev-cond-result
+  (assign unev (op cond-actions) (reg exp))
+  (goto (label ev-sequence))
+ev-cond-else
+  (goto (label ev-cond-result))
 
 ev-assignment
   (assign unev (op assignment-variable) (reg exp))
